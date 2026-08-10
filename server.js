1b878beae8d4a237f45c1699b9de0ac6e5c23c0a
mineshrathwa46-stack <mineshrathwa46@gmail.com>
2026-08-10 14:26:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://ai-chatbot-eta-five-22.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
