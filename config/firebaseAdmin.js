67366a074b38aad14a72024c9eb68ef0d374fe1a
mineshrathwa46-stack <mineshrathwa46@gmail.com>
2026-08-10 14:33:26 +0530

    Refactor Firebase Admin initialization and exports

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,13 +1,30 @@
-const admin = require("firebase-admin");
+const { initializeApp, getApps, cert } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
+const { getFirestore } = require("firebase-admin/firestore");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
+const firebaseConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
+};
+
+let app;
+
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert(firebaseConfig),
   });
+} else {
+  app = getApps()[0];
 }
 
-module.exports = admin;
+const adminAuth = getAuth(app);
+const db = getFirestore(app);
+
+module.exports = {
+  admin: app,
+  adminAuth,
+  db,
+};
